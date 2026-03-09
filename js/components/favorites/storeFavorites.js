d2162b11883ef39657c4e10e37757b2058ca2374
albina_biriukova <Alja.birjukova@gmail.com>
2026-03-09 11:49:53 +0100

    Update storeFavorites.js

--- js/components/favorites/storeFavorites.js
+++ js/components/favorites/storeFavorites.js
@@ -1,8 +1,12 @@
 const STORAGE_KEY = "favorites";
 
-let favorites = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+export const getAllFavorites = () => {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+};
 
 export const addToFav = (user) => {
+  const favorites = getAllFavorites();
+
   const exist = favorites.find((fav) => fav.id === user.id);
 
   if (!exist) {
@@ -12,11 +16,9 @@ export const addToFav = (user) => {
 };
 
 export const removeFromFav = (id) => {
+  let favorites = getAllFavorites();
+
   favorites = favorites.filter((user) => user.id !== id);
 
   localStorage.setItem(STORAGE_KEY, JSON.stringify(favorites));
-};
-
-export const getAllFavorites = () => {
-  return favorites;
 };

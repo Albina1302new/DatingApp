5a6c712ee3b90b47229ea14fb1d74fd1aef4b3b3
albina_biriukova <Alja.birjukova@gmail.com>
2026-03-09 11:42:39 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import { renderHeader } from "./components/nav.js";
 import { hero } from "./components/hero.js";
-import { renderFavorites } from "./components/favorites/renderFavorites.js";
+import { renderFavorites } from "./favorites/renderFavorites.js";
 import { renderFakeMessages } from "./components/dialog/renderMessanges.js";
 import { renderJulia } from "./components/renderJulia.js";
 
@@ -17,9 +17,9 @@ const favContainer = document.querySelector(".fav__container");
 if (favContainer) {
   renderFavorites();
 }
-if (document.querySelector(".fav__container")) {
+/* if (document.querySelector(".fav__container")) {
   renderFavorites();
-}
+} */
 if (window.location.pathname.includes("dialog.html")) {
   renderFakeMessages();
 }
